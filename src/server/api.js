f008e1f14246555ed3d2aa070b5b0ce68fa1e3c9
Kat Christensen <katchristensen336@gmail.com>
2023-12-19 14:39:44 -0500

    Update get all levels

--- src/server/api.js
+++ src/server/api.js
@@ -4,7 +4,7 @@ const apiRouter = express.Router();
 const { requireAdmin, requireUser } = require("./utils")
 
 const { PrismaClient } = require("@prisma/client");
-const { recipeBookItem, recipeBook } = require('./client');
+const { recipeBookItem, recipeBook, level } = require('./client');
 const prisma = new PrismaClient();
 
 //<-----------------GET ALL RECIPES----------------->
@@ -20,19 +20,27 @@ apiRouter.get("/recipes", async (req, res, next) => {
 //<-----------------GET SINGLE RECIPE----------------->
 //GET /api/recipe/:id
 apiRouter.get("/recipe/:id", async (req, res, next) => {
-try{
-    const recipe = await prisma.recipe.findUnique({
-        where: {
-            id: Number(req.params.id)
-        }
-    });
-    res.send(recipe);
-} catch (error){
-    next(error);
-}
+    try {
+        const recipe = await prisma.recipe.findUnique({
+            where: {
+                id: Number(req.params.id)
+            }
+        });
+        res.send(recipe);
+    } catch (error) {
+        next(error);
+    }
 })
 //<-----------------GET ALL LEVELS----------------->
-
+//GET /api/levels
+apiRouter.get("/levels", async (req, res, next) => {
+    try {
+        const levels = await prisma.level.findMany();
+        res.send(levels);
+    } catch (error) {
+        next(error);
+    }
+})
 //<-----------------GET SINGLE LEVELS----------------->
 
 //<-----------------GET RECIPES BY LEVEL----------------->
