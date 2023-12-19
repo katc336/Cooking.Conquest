cefa5d0155757b29ad83b51c8ab4a864aedbdf3a
Kat Christensen <katchristensen336@gmail.com>
2023-12-19 14:06:12 -0500

    added register and dotenv, morgan, and jsonwebtoken

--- src/server/auth.js
+++ src/server/auth.js
@@ -4,6 +4,8 @@ const authRouter = express.Router();
 const { requireAdmin, requireUser } = require("./utils")
 
 const jwt = require("jsonwebtoken")
+
+require("dotenv").config();
 const { JWT_SECRET } = process.env
 
 const { PrismaClient } = require("@prisma/client");
@@ -12,6 +14,27 @@ const prisma = new PrismaClient();
 const bcrypt = require("bcrypt");
 const SALT_COUNT = 10;
 
-//TO DO 
+//<--------------------------------REGISTER USER-------------------------------->
+// POST /auth/register
+authRouter.post("/register", async (req, res, next) => {
+    try {
+        const { name, username, email, password, } = req.body
+        const hashedPassword = await bcrypt.hash(password, SALT_COUNT)
+
+        const user = await prisma.user.create({
+            data: {
+                name: name,
+                username: username,
+                email: email,
+                password: hashedPassword
+            }
+        });
+        const token = jwt.sign({ id: user.id, username }, process.env.JWT_SECRET);
+        delete user.password
+        res.send({ user, token });
+    } catch (error) {
+        next(error)
+    }
+})
 
 module.exports = authRouter;
