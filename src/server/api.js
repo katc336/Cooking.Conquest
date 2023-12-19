87c92af3fca8ebe7b5185cc48a87527dbbde7e39
Kat Christensen <katchristensen336@gmail.com>
2023-12-19 14:31:14 -0500

    add GET all recipes

--- src/server/api.js
+++ src/server/api.js
@@ -7,6 +7,35 @@ const { PrismaClient } = require("@prisma/client");
 const { recipeBookItem, recipeBook } = require('./client');
 const prisma = new PrismaClient();
 
-//TO DO 
+//<-----------------GET ALL RECIPES----------------->
+// GET /api/recipes
+apiRouter.get("/recipes", async (req, res, next) => {
+    try {
+        const recipes = await prisma.recipe.findMany();
+        res.send(recipes);
+    } catch (error) {
+        next(error);
+    }
+})
+//<-----------------GET SINGLE RECIPE----------------->
 
+//<-----------------GET ALL LEVELS----------------->
+
+//<-----------------GET SINGLE LEVELS----------------->
+
+//<-----------------GET RECIPES BY LEVEL----------------->
+
+//<-----------------ADD RECIPE TO USER ACCOUNT----------------->
+
+//<-----------------GET ALL USER'S RECIPES----------------->
+
+//<-----------------GET SINGLE USER'S RECIPE----------------->
+
+//<-----------------DELETE USER'S RECIPES----------------->
+
+//<-----------------PATCH RECIPES----------------->
+//NOTE: ADMIN ONLY
+
+//<-----------------DELETE RECIPES----------------->
+//NOTE: ADMIN ONLY
 module.exports = apiRouter;
