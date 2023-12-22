38110e140a001bc9a32dc4fb00d318d584910012
Kat Christensen <katchristensen336@gmail.com>
2023-12-21 23:52:06 -0500

    Update with adding redux toolkit with authentication

---
+++ src/redux/api.js
@@ -0,0 +1,64 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const backendURL = "/";
+
+const api = createApi({
+    reducerPath: 'api',
+
+    baseQuery: fetchBaseQuery({
+        baseUrl: backendURL,
+
+        prepareHeaders: (headers, { getState }) => {
+            const token = getState().auth.token
+            if (token) {
+                headers.set('Authorization', `Bearer ${token}`)
+            }
+            return headers
+        },
+    }),
+    tagTypes: ["Users"],
+
+    endpoints: (builder) => ({
+        //<-----------AUTHORIZATION----------->
+        //REGISTER ACCOUNT ENDPOINT
+        register: builder.mutation({
+            query: (user) => ({
+                url: `/auth/register`,
+                method: 'POST',
+                body: user,
+            }),
+            providesTags: ["Users"]
+        }),
+        //LOGIN ACCOUNT 
+        login: builder.mutation({
+            query: (user) => ({
+                url: `/auth/login`,
+                method: 'POST',
+                body: user,
+            }),
+            providesTags: ["Users"]
+        }),
+        //<------------GET USER INFO------------>
+        //GET USER
+        getUser: builder.query({
+            query: () => ({
+                url: `/auth/account`,
+                method: 'GET',
+            }),
+            providesTags: ["Users"]
+        }),
+     
+      
+       
+    }),
+})
+export default api;
+
+export const {
+    //AUTHORIZATION
+    useRegisterMutation,
+    useLoginMutation,
+    useLogoutMutation,
+    //GET USER'S INFO
+    useGetUserQuery,
+} = api
