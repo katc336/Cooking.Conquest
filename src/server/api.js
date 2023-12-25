e3ba0b67e1bd9166c92088e79b65b6ce76b2d69c
Kat Christensen <katchristensen336@gmail.com>
2023-12-24 22:45:00 -0500

    Update backend with GET recipes by level

--- src/server/api.js
+++ src/server/api.js
@@ -70,7 +70,69 @@ apiRouter.get("/recipes/:levelId", async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-})
+});
+//<-----------------GET LEVEL 1 RECIPES----------------->
+//GET /api/recipes/level-one
+apiRouter.get("/recipes-level-one", async (req, res, next) => {
+    try {
+        // Find level one 
+        const levelOne = await prisma.level.findFirst({
+            where: { level: "one" }
+        });
+        // Find recipes from level one
+        const levelOneRecipes = await prisma.recipe.findMany({
+            where: {
+                levelId: levelOne.id
+            }
+        });
+        res.send(levelOneRecipes);
+    } catch (error) {
+        next(error)
+    }
+});
+//<-----------------GET LEVEL 2 RECIPES----------------->
+//GET /api/recipes/level-two
+apiRouter.get("/recipes-level-two", async (req, res, next) => {
+    try {
+        // Find level two
+        const levelTwo = await prisma.level.findFirst({
+            where: {
+                level: "two"
+            }
+        });
+        // Find recipes from level two
+        const levelTwoRecipes = await prisma.recipe.findMany({
+            where: {
+                levelId: levelTwo.id
+            }
+        });
+        res.send(levelTwoRecipes);
+    } catch (error) {
+        next(error)
+    }
+});
+//<-----------------GET LEVEL 3 RECIPES----------------->
+//GET /api/recipes/level-three
+apiRouter.get("/recipes-level-three", async (req, res, next) => {
+    try {
+        // Find level three
+        const levelThree = await prisma.level.findFirst({
+            where: {
+                level: "three"
+            }
+        });
+        // Find recipes from level three
+        const levelThreeRecipes = await prisma.recipe.findMany({
+            where: {
+                levelId: levelThree.id
+            }
+        });
+        res.send(levelThreeRecipes);
+
+    } catch (error) {
+        next(error)
+    }
+});
 //<-----------------ADD RECIPE TO USER ACCOUNT----------------->
 apiRouter.post("/myRecipe", requireUser, async (req, res, next) => {
     const recipeId = req.body.recipeId
