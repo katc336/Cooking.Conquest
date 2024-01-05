5e6a9142666c92671d958abccc21b6eebe67944f
Kat Christensen <katchristensen336@gmail.com>
2024-01-04 20:34:01 -0500

    Update recipe level blocked conditions

--- src/client/SeeRecipes/LevelThreeRecipes.jsx
+++ src/client/SeeRecipes/LevelThreeRecipes.jsx
@@ -2,12 +2,14 @@ import { Box, Card, Typography, Stack, Tooltip } from "@mui/material"
 
 import { Link } from "react-router-dom"
 
-import { useGetLevelThreeRecipesQuery } from "../../redux/api"
+import { useGetUserQuery, useGetLevelThreeRecipesQuery } from "../../redux/api"
 
+import NoUserRecipeView from "./NoUserRecipeView"
 import RecipeCircle from "./RecipeCircle.png"
 
 const LevelThreeRecipes = () => {
     const { data, error, isLoading } = useGetLevelThreeRecipesQuery();
+    const { data: userData, error: userError, isLoading: userIsLoading } = useGetUserQuery();
     if (data) {
         console.log(data)
     }
@@ -16,37 +18,57 @@ const LevelThreeRecipes = () => {
     }
     return (
         <div>
-            <Box sx={{ ml: 3, px: "10%", backgroundColor: "transparent" }}>
-                <Typography
-                    variant="h5"
-                    sx={{ mt: 10, textAlign: "center", color: "#362706", fontWeight: "bold" }}>
-                    Level Three:
-                </Typography>
-                <Stack direction="row" useFlexGap flexWrap="wrap">
-                    {data && data.map((recipes) => (
-                        <Tooltip title="Click to see recipe details">
-                            <Box
-                                sx={{ my: 5 }}
-                                style={{
-                                    backgroundImage: `url(${RecipeCircle})`,
-                                    width: "150px",
-                                    height: "150px",
-                                    backgroundSize: "contain",
-                                    backgroundRepeat: "no-repeat",
-                                }}>
-                                <Stack direction="column">
-                                    <Typography sx={{ mt: "30%", mx: "20%", fontWeight: "bold", textAlign: "center", color: "#935900" }}>
-                                        {recipes.name}
-                                    </Typography>
-                                    <Typography sx={{ textAlign: "center" }}>
-                                    </Typography>
-                                </Stack>
-                            </Box>
-                        </Tooltip>
-                    ))
-                    }
-                </Stack>
-            </Box>
+            {!userData || userData.level !== 3
+                ? //if no user or not a high enough level...
+                <div>
+                    <Stack direction="column">
+                        <Typography
+                            variant="h5"
+                            sx={{ mt: 10, textAlign: "center", color: "#362706", fontWeight: "bold" }}>
+                            Level three quests are Locked!
+                        </Typography>
+                        <Typography
+                            variant="h6"
+                            sx={{ textAlign: "center", color: "#362706", fontWeight: "bold" }}>
+                            Sign up or level up to unlock level three recipes!
+                        </Typography>
+                    </Stack>
+                    <NoUserRecipeView />
+                </div>
+                : //if user...
+                <div>
+                    <Box sx={{ ml: 3, px: "10%", backgroundColor: "transparent" }}>
+                        <Typography
+                            variant="h5"
+                            sx={{ mt: 10, textAlign: "center", color: "#362706", fontWeight: "bold" }}>
+                            Level Three:
+                        </Typography>
+                        <Stack direction="row" useFlexGap flexWrap="wrap">
+                            {data && data.map((recipes) => (
+                                <Tooltip title="Click to see recipe details">
+                                    <Box
+                                        sx={{ my: 5 }}
+                                        style={{
+                                            backgroundImage: `url(${RecipeCircle})`,
+                                            width: "150px",
+                                            height: "150px",
+                                            backgroundSize: "contain",
+                                            backgroundRepeat: "no-repeat",
+                                        }}>
+                                        <Stack direction="column">
+                                            <Typography sx={{ mt: "30%", mx: "20%", fontWeight: "bold", textAlign: "center", color: "#935900" }}>
+                                                {recipes.name}
+                                            </Typography>
+                                            <Typography sx={{ textAlign: "center" }}>
+                                            </Typography>
+                                        </Stack>
+                                    </Box>
+                                </Tooltip>
+                            ))
+                            }
+                        </Stack>
+                    </Box>
+                </div>}
         </div >
     )
 }
--- src/client/SeeRecipes/NoUserRecipeView.jsx
+++ src/client/SeeRecipes/NoUserRecipeView.jsx
@@ -7,22 +7,13 @@ import LockedRecipeIcon from "./LockedRecipeIcon.png"
 const NoUserRecipeView = () => {
     return (
         <div>
-            <Box sx={{ mx: "15%", my: 1 }}>
-                <Link to="/register">
-                <Button sx={{ textTransform: "none", backgroundColor: "transparent" }}>
-                    <Stack direction="column">
-                        <Typography
-                            variant="h6"
-                            sx={{ textAlign: "center", color: "#362706", fontWeight: "bold", my: 1 }}>
-                            Sign up to unlock more recipe quests!
-                        </Typography>
-                        <Box sx={{ mx: "15%" }}>
-                            <img src={LockedRecipeIcon} alt="Locked Icon" width="100px" />
-                        </Box>
-                    </Stack>
-                </Button>
-                </Link>
-            </Box>
+            <Typography sx={{ textAlign: "center" }}>
+                <Box>
+                    <img src={LockedRecipeIcon}
+                        alt="Locked Icon"
+                        width="200px" />
+                </Box>
+            </Typography>
         </div>
     )
 }
