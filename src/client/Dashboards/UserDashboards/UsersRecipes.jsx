d62e14f58a534bbe772869062f1dbf6931a04ae9
Kat Christensen <katchristensen336@gmail.com>
2024-01-10 20:08:42 -0500

    Add UserRecipe recipe item display

--- src/client/Dashboards/UserDashboards/UsersRecipes.jsx
+++ src/client/Dashboards/UserDashboards/UsersRecipes.jsx
@@ -1,15 +1,16 @@
-import Typography from "@mui/material/Typography"
-import Box from "@mui/material/Box"
-import Button from "@mui/material/Button"
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import { Link } from "react-router-dom";
+import { useGetRecipeBookItemQuery } from "../../../redux/api";
 
-import { Link } from "react-router-dom"
-
-import { useGetRecipeBookItemQuery } from "../../../redux/api"
-
+import CloudBox from "./images/CloudBox.png"
 const UsersRecipes = () => {
     const { data, error, isLoading } = useGetRecipeBookItemQuery();
     if (isLoading) {
         console.log("Loading...")
+        //TO DO
         return null
     }
     if (data) {
@@ -20,41 +21,78 @@ const UsersRecipes = () => {
     }
     return (
         <div>
-            {data
-                ? //if there is a recipe book item...
+            {data && data.length > 0
+                ? // If there are recipe book items...
                 <div>
-               
+                    <Stack direction="column">
+                        <Box sx={{ mt: "18%", mx: "20%" }}>
+                            {data.map((recipeBookItem) => (
+                                <div key={recipeBookItem.id}>
+                                    <Box sx={{ backgroundColor: "#F1E4C3", px: 2, py: 1, my: 1, border: 2, borderColor: "#445D48", borderBottom: 5, borderRadius: "20px" }}>
+                                        <Stack direction="row">
+                                        <Typography
+                                            variant="h4"
+                                            sx={{ pt: 1, mx: 2}}>
+                                            {recipeBookItem.recipe.name}
+                                        </Typography>
+                                        <Button sx={{
+                                            m: 1,
+                                            color: "#445D48",
+                                            borderRadius: "10px",
+                                            border: 2,
+                                            borderBottom: 5,
+                                            borderColor: "#445D48",
+                                            textTransform: "none"
+                                        }}>
+                                            View Recipe
+                                        </Button>
+                                        <Button sx={{
+                                             m: 1,
+                                             color: "white",
+                                             borderRadius: "10px",
+                                             backgroundColor: "#65B741",
+                                             border: 2,
+                                             borderBottom: 5,
+                                             borderColor: "#445D48",
+                                             textTransform: "none"
+                                        }}>
+                                            Click to here completed the quest
+                                        </Button>
+                                        </Stack>
+                                    </Box>
+                                </div>
+                            ))}
+                        </Box>
+                    </Stack>
                 </div>
-                : //there are no recipes...
-                <div>
-                         <Box sx={{ mt: "20%", mx: "10%", p: 3, backgroundColor: "rgba(223, 233, 228, 1)", border: 2, borderColor: "#445D48", borderBottom: 5, borderRadius: "20px" }}>
-                        <Typography
-                            variant="h4"
-                            sx={{ fontWeight: "bold", textAlign: "center", color: "#362706" }}>
-                            Want to find recipes to start a cooking quest?
-                        </Typography>
-                        <Typography sx={{ textAlign: "center" }}>
-                            <Link to="/recipes">
-                                <Button
-                                    sx={{
-                                        m: 1,
-                                        fontSize: "20px",
-                                        color: "white",
-                                        borderRadius: "10px",
-                                        backgroundColor: "#65B741",
-                                        border: 2,
-                                        borderBottom: 5,
-                                        borderColor: "#445D48",
-                                        textTransform: "none"
-                                    }}>
-                                    Click here to explore new recipes!
-                                </Button>
-                            </Link>
-                        </Typography>
-                    </Box>
-                </div>
+                : //If there are no recipe book items..
+                <Box sx={{ mt: "20%", mx: "10%", p: 3, backgroundColor: "rgba(223, 233, 228, 1)", border: 2, borderColor: "#445D48", borderBottom: 5, borderRadius: "20px" }}>
+                    <Typography variant="h4" sx={{ fontWeight: "bold", textAlign: "center", color: "#362706" }}>
+                        Want to find recipes to start a cooking quest?
+                    </Typography>
+                    <Typography sx={{ textAlign: "center" }}>
+                        <Link to="/recipes">
+                            <Button
+                                sx={{
+                                    m: 1,
+                                    fontSize: "20px",
+                                    color: "white",
+                                    borderRadius: "10px",
+                                    backgroundColor: "#65B741",
+                                    border: 2,
+                                    borderBottom: 5,
+                                    borderColor: "#445D48",
+                                    textTransform: "none"
+                                }}
+                            >
+                                Click here to explore new recipes!
+                            </Button>
+                        </Link>
+                    </Typography>
+                </Box>
             }
         </div>
-    )
-}
-export default UsersRecipes
+    );
+};
+
+export default UsersRecipes;
