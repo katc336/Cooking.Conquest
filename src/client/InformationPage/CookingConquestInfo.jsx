7c9cb5954bd59303507fd0f902d3e8aae638dee2
Kat Christensen <katchristensen336@gmail.com>
2024-01-13 13:44:37 -0500

    Update information page

--- src/client/InformationPage/CookingConquestInfo.jsx
+++ src/client/InformationPage/CookingConquestInfo.jsx
@@ -1,5 +1,6 @@
 import Box from "@mui/material/Box"
 import Button from "@mui/material/Button"
+import Card from "@mui/material/Card"
 import Grid from "@mui/material/Grid"
 import Typography from "@mui/material/Typography"
 import Stack from "@mui/material/Stack"
@@ -23,10 +24,14 @@ const CookingConquestInfo = () => {
                     backgroundRepeat: "no-repeat",
                 }}>
                 {/* <-------------------Row 1-------------------> */}
+               <Typography
+               variant="h3"
+               sx={{ color: "#362706", textAlign: "center", mb: 2 }}>
+               Welcome to Cooking Conquest!
+               </Typography>
                 <Typography
                     variant="h6"
                     sx={{ color: "#362706", mb: 2 }}>
-                    Welcome to Cooking Conquest!
                     Youare about to embark on an exciting journey to learn and conquer the world of vegetarian cooking.
                     This app is designed to make your cooking experience interactive and fun,
                     all while helping you develop new skills and knowledge.
@@ -44,17 +49,26 @@ const CookingConquestInfo = () => {
                     sx={{ color: "#362706", mb: 2 }}>
                     Once you reach level 3, you can join a "guild" within the app community.
                     Guilds are groups of users who collaborate to create and share new vegetarian recipes.
-                    Joining a guild allows you to compete with others by reviewing and rating each other's recipes, earning points,
-                    and contributing to your guild’s cumulative score.
+                    {/* Joining a guild allows you to compete with others by reviewing and rating each other's recipes, earning points,
+                    and contributing to your guild’s cumulative score. */}
                 </Typography>
+                <Card sx={{ border: 5, borderColor: "#862B0D", backgroundColor: "#FFF9C9", m: 3, p: 2 }}>
                 <Typography
-                    variant="h6"
-                    sx={{ color: "#362706" }}>
-                    COMING SOON:  keep an eye out for the leaderboard to see how your guild and individual cooking skills rank.
-                    This friendly competition and sense of camaraderie within the community will keep you motivated
-                    and engaged in your vegetarian cooking conquest.
-                </Typography>
-                <Box sx={{ mx:"5%" }}>
+                        variant="h5"
+                        sx={{ color: "#362706", textAlign: "center" }}>
+                        COMING SOON: 
+                    </Typography>
+                    <Typography
+                        variant="h6"
+                        sx={{ color: "#362706" }}>
+                         Keep an eye out for the leaderboard to see how your guild and individual cooking skills rank!
+                        Once tou join a guild, you can compete with others by reviewing and rating each other's recipes, earning points,
+                        and contributing to your guild’s cumulative score.
+                        This friendly competition and sense of camaraderie within the community will keep you motivated
+                        and engaged in your vegetarian cooking conquest.
+                    </Typography>
+                </Card>
+                <Box sx={{ mx: "5%" }}>
                     <Stack direction="row">
                         <img
                             src={SingleRecipePageView}
@@ -67,31 +81,31 @@ const CookingConquestInfo = () => {
                         </Typography>
                     </Stack>
                 </Box>
-                <Box sx={{ mx:"5%" }}>
-                <Stack direction="row">
-                    <img
-                        src={LockedLevelPageView}
-                        alt="Page view of a locked level"
-                        width="550px" />
-                    <Typography
-                        variant="h6"
-                        sx={{ color: "#362706", mx: 1, mt: 15, mb: 1 }}>
-                        Some recipes are locked until you reach that recipe's level
-                    </Typography>
-                </Stack>
+                <Box sx={{ mx: "5%" }}>
+                    <Stack direction="row">
+                        <img
+                            src={LockedLevelPageView}
+                            alt="Page view of a locked level"
+                            width="550px" />
+                        <Typography
+                            variant="h6"
+                            sx={{ color: "#362706", mx: 1, mt: 15, mb: 1 }}>
+                            Some recipes are locked until you reach that recipe's level
+                        </Typography>
+                    </Stack>
                 </Box>
-                <Box sx={{ mx:"1%" }}>
-                <Stack direction="row">
-                    <img
-                        src={AddRecipePageView}
-                        alt="Page view of how to add a recipe"
-                        width="600px" />
-                    <Typography
-                        variant="h6"
-                        sx={{ color: "#362706", mt: 15, mx: 1, mb: 3 }}>
-                        Click on the add recipe button to add the recipe to your account.
-                    </Typography>
-                </Stack>
+                <Box sx={{ mx: "1%" }}>
+                    <Stack direction="row">
+                        <img
+                            src={AddRecipePageView}
+                            alt="Page view of how to add a recipe"
+                            width="600px" />
+                        <Typography
+                            variant="h6"
+                            sx={{ color: "#362706", mt: 15, mx: 1, mb: 3 }}>
+                            Click on the add recipe button to add the recipe to your account.
+                        </Typography>
+                    </Stack>
                 </Box>
                 <Box sx={{ mt: 3 }}>
                     <Stack direction="row">
