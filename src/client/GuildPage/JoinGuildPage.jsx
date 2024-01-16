a31b5ce5e8201d70a432cc7c3b5efe58f39e59fb
Kat Christensen <katchristensen336@gmail.com>
2024-01-16 17:41:15 -0500

    Update join guild page

--- src/client/GuildPage/JoinGuildPage.jsx
+++ src/client/GuildPage/JoinGuildPage.jsx
@@ -9,12 +9,14 @@ import { useParams, useNavigate } from "react-router-dom"
 
 import { useGetAllGuildsQuery, usePatchJoinGuildMutation } from "../../redux/api"
 
+import GuildSelectBackground from "./images/GuildSelectBackground.png"
+
 const JoinGuildPage = () => {
     const { data, error, isLoading } = useGetAllGuildsQuery()
     const { id } = useParams();
     const [patchGuild] = usePatchJoinGuildMutation(id);
     const navigate = useNavigate();
-    
+
     if (isLoading) {
         console.log("Loading...")
         return null
@@ -36,54 +38,64 @@ const JoinGuildPage = () => {
     };
     return (
         <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1, ease: "easeIn" }}>
-            <Box sx={{ mt: "10%" }}>
-                <Stack direction="row">
-                    {data && data.map((guild) => (
-                        <div key={guild.id}>
-                            <Box sx={{
-                                p: 1,
-                                m: 1,
-                                maxWidth: "300px",
-                                minHeight: "330px",
-                                color: "#445D48",
-                                borderRadius: "10px",
-                                backgroundColor: "transparent",
-                                border: 2,
-                                borderBottom: 5,
-                                borderColor: "rgba(80, 125, 106, 1)",
-                            }}>
-                                <Typography
-                                    variant="h6"
-                                    sx={{ textAlign: "center", }}>
-                                    {guild.name}
-                                </Typography>
-                                <Typography>
-                                    {guild.description}
-                                </Typography>
-                                <Button
-                                    onClick={() => handlePatch(guild.id)}
-                                    sx={{
-                                        p: 1,
-                                        my: 1,
-                                        fontSize: "20px",
-                                        fontFamily: "Tahoma",
-                                        color: "white",
-                                        borderRadius: "10px",
-                                        backgroundColor: "#65B741",
-                                        border: 2,
-                                        borderBottom: 5,
-                                        borderColor: "#445D48",
-                                        textTransform: "none"
-                                    }}>
-                                    Join Guild
-                                </Button>
-                            </Box>
-                        </div>
-                    ))}
-                </Stack>
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, ease: "easeIn" }}>
+            <Box sx={{ mt: "10%", mx: "10%" }}>
+                <img
+                    src={GuildSelectBackground}
+                    style={{ position: "absolute", zIndex: -1 }}
+                    width="1200px"
+                />
+                <Box sx={{ px: 10}}>
+                    <Stack direction="row">
+                        {data && data.map((guild) => (
+                            <div key={guild.id}>
+                                <Box sx={{
+                                    p: 2,
+                                    mx: 1,
+                                    mt: 20,
+                                    maxWidth: "300px",
+                                    minHeight: "300px",
+                                    color: "#445D48",
+                                    backgroundColor: "rgba(223, 233, 228, 1)",
+                                    borderRadius: "10px",
+                                    border: 2,
+                                    borderBottom: 5,
+                                    borderColor: "rgba(80, 125, 106, 1)",
+                                }}>
+                                    <Typography
+                                        variant="h6"
+                                        sx={{ textAlign: "center", mb: 2 }}>
+                                        {guild.name}
+                                    </Typography>
+                                    <Typography>
+                                        {guild.description}
+                                    </Typography>
+                                </Box>
+                                <Typography sx={{ textAlign: "center" }}>
+                                    <Button
+                                        onClick={() => handlePatch(guild.id)}
+                                        sx={{
+                                            p: 1,
+                                            my: 1,
+                                            fontSize: "20px",
+                                            fontFamily: "Tahoma",
+                                            color: "white",
+                                            borderRadius: "10px",
+                                            backgroundColor: "#65B741",
+                                            border: 2,
+                                            borderBottom: 5,
+                                            borderColor: "#445D48",
+                                            textTransform: "none"
+                                        }}>
+                                        Join Guild
+                                    </Button>
+                                    </Typography>
+                            </div>
+                        ))}
+                    </Stack>
+                </Box>
             </Box>
         </motion.div>
     )
