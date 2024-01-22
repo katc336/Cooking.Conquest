eaf819c23ea1c5350ab35dd50745a432fa82c148
Kat Christensen <katchristensen336@gmail.com>
2024-01-21 20:29:39 -0500

    Improve ux experience with recipe already added

--- src/client/SeeRecipes/SingleRecipePage/AddRecipeButton.jsx
+++ src/client/SeeRecipes/SingleRecipePage/AddRecipeButton.jsx
@@ -1,4 +1,5 @@
 import Button from "@mui/material/Button";
+import Box from "@mui/material/Box"
 import Typography from "@mui/material/Typography";
 import Snackbar from '@mui/material/Snackbar';
 
@@ -37,33 +38,45 @@ const AddRecipeButton = () => {
 
     return (
         <div>
-            {data
+            {data && data.level !== 4
                 ? //If the user is not level 4 and does not already have the recipe added, return the button
-                (data.level !== 4 && !checkIfAdded
-                    && <Typography sx={{ textAlign: "center" }}>
-                        <Button
-                            onClick={(event) => handlePost(event)}
-                            variant="contained"
-                            color="success"
-                            sx={{
-                                mx: 1,
-                                mt: 1,
-                                color: "white",
-                                borderRadius: "10px",
-                                backgroundColor: "#65B741",
-                                border: 2,
-                                borderBottom: 5,
-                                borderColor: "#445D48",
-                                textTransform: "none",
-                            }} >
-                            Add Recipe to My Recipe Book
-                        </Button>
-                    </Typography>
-                )
-                : (
-                    // if not a user, return an empty div
-                    <div />
-                )}
+                <div>
+                    {checkIfAdded === true
+                        ? //if the user already has this recipe...
+                        <div>
+                            <Box sx={{ mt: 3, border: 5, borderColor: "#862B0D", borderRadius: "20px", backgroundColor: "#FFF9C9", mx: 18, p: 1 }}>
+                                <Typography sx={{ textAlign: "center", color: "#862B0D", fontWeight: "bold" }}>
+                                    This recipe is already added to your recipe spellbook
+                                </Typography>
+                            </Box>
+                        </div>
+                        : // if this user does not have this recipe added...
+                        <div>
+                            <Typography sx={{ textAlign: "center" }}>
+                                <Button
+                                    onClick={(event) => handlePost(event)}
+                                    variant="contained"
+                                    color="success"
+                                    sx={{
+                                        mx: 1,
+                                        mt: 1,
+                                        color: "white",
+                                        borderRadius: "10px",
+                                        backgroundColor: "#65B741",
+                                        border: 2,
+                                        borderBottom: 5,
+                                        borderColor: "#445D48",
+                                        textTransform: "none",
+                                    }} >
+                                    Add Recipe to My Recipe Book
+                                </Button>
+                            </Typography>
+                        </div>
+                    }
+                </div>
+                : // if not a user, return an empty div
+                <div />
+            }
         </div>
     );
 };
